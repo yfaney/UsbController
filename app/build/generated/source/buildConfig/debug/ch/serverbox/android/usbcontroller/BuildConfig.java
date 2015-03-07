a1ba13aa52f5d40d114235f917ceea18bf4dedb1
Younghwan Jang <yjvxf@mail.umkc.edu>
2015-03-06 23:38:55 -0600

    First Commit

---
+++ app/build/generated/source/buildConfig/debug/ch/serverbox/android/usbcontroller/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package ch.serverbox.android.usbcontroller;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String PACKAGE_NAME = "ch.serverbox.android.usbcontroller";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "";
+}
